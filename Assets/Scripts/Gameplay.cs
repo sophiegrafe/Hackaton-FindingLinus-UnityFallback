270f237b755682b8e34cf06bbc16d2d2eda22c04
Grafé <Sophie>
2020-11-28 16:25:26 +0100

    first commit

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+	
+	public GameObject target;
+
+	private void PlayAgain()
+	{	
+		target.active = true;
+		Debug.Log("Play Again!");
+	}
+}
---
+++ Assets/Scripts/touchMe.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class touchMe : MonoBehaviour
+{
+	
+	[DllImport("__Internal")]
+    private static extern void HelloString(string str);
+	
+	[DllImport("__Internal")]
+    private static extern void showEnigma(string str);
+
+	//public string myMsg = "Don't touch me!";
+	public GameObject target;
+	public AudioSource audioData;
+	
+	private void OnTriggerEnter(Collider other)
+	{	
+		if (other.CompareTag("Player"))
+		{
+			target.active = false;
+			audioData.Play(0);
+			showEnigma("You touched me!");
+			Debug.Log("Touched!");
+		}
+	}
+}
