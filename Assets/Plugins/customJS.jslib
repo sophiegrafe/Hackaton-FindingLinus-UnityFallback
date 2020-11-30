20c7e58dd10634ea7b0760b1f6241e7d3bbd9f27
Grafé <Sophie>
2020-11-30 09:06:43 +0100

    ajout gameControl pour activation clavier lorsqu'on est dans la partie web et retour au jeu au click sur le canva jeu

---
+++ Assets/Plugins/customJS.jslib
@@ -0,0 +1,13 @@
+mergeInto(LibraryManager.library, {
+  // HelloString: function (str) {
+  //   window.alert(Pointer_stringify(str));
+  // },
+
+  // GameMessage: function (evtsrc,evtval) {
+  //   window.GameMessage(Pointer_stringify(evtsrc),Pointer_stringify(evtval));
+  // },
+
+  showEnigma: function (objectName) {
+    window.showEnigma(Pointer_stringify(objectName));
+  },
+});
---
+++ Assets/Scripts/clickMe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class clickMe : MonoBehaviour
+{
+	
+	[DllImport("__Internal")]
+    private static extern void showEnigma(string str);
+	
+	public string objectName;
+	public AudioSource audioData;
+	
+	private void OnMouseDown()
+	{	
+		//this.setActive = false;
+		audioData.Play(0);
+		Debug.Log(objectName);
+		showEnigma(objectName);		
+	}
+}
